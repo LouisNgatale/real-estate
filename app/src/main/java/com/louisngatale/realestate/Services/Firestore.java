acd2a1b6d97a4843e16986ed57fdb26eab3b5c1a
LouisNgatale <louisngatale@gmail.com>
2021-04-03 21:19:10 +0300

    Firestore Service class functionalities

--- app/src/main/java/com/louisngatale/realestate/Services/Firestore.java
+++ app/src/main/java/com/louisngatale/realestate/Services/Firestore.java
@@ -1,4 +1,63 @@
 package com.louisngatale.realestate.Services;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.louisngatale.realestate.Screens.Map;
+
 public class Firestore {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final String TAG = "Firestore";
+
+
+    //    Add new house
+    public void addHouse(Map house){
+        db.collection("houses")
+                .add(house)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+    }
+
+
+//    Get all houses
+
+
+
+//    Get one house
+    public void getHouse(){
+        db.collection("houses")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+    }
 }
